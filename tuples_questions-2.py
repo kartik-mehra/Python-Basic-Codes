13f0b7c284844cb846a4ae87d980349cddccdc0d
Kartik mehra <kartikmhr97@gmail.com>
2017-09-25 11:37:11 +0530

    questions on tuples

---
+++ tuples_questions-2.py
@@ -0,0 +1,26 @@
+# tuples programs
+
+#WAP to swap two values using tuple assignment
+val1=10
+val2=20
+print("val1= ",val1,"val2=",val2)
+(val1,val2)=(val2,val1)
+print("******")
+print("val1= ",val1,"val2=",val2)
+
+#WAP that scans an email address and forms a tuple of user name and domain name.
+print("Enter the email address:")
+add=input("Enter the email address:")
+username,domain_name=add.split("@")
+print("Username = ",username)
+print("Domain name = ",domain_name)
+
+#WAP that has two sequences. First which stores some questions and second stores the corresponding answers.
+#Use zip() function to form avalid question answer series.
+ques=["Roll_no","name","course"]
+ans=["18","vidhi","CSE"]
+for i,j in zip(ques,ans):
+    print("What is your ",i," ?")
+    print("My ",i,"is : ",j)
+          
+      
